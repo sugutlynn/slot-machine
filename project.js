2746ad583fc63f9b8e46570f5caa6094688d3c6f
sugutlynn <sugutlynn@gmail.com>
2023-10-24 00:49:12 +0300

    new project steps

---
+++ project.js
@@ -0,0 +1,7 @@
+//1. Deposit money
+//2.Determine number of lines on bet
+//3.Collect a bet amount
+//4. Spin the slot machine
+//5. check if the user won
+//6. give the user their winnings
+//7. play again
