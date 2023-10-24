b20e9616e6114994195750674333a1a1acc80299
sugutlynn <sugutlynn@gmail.com>
2023-10-24 23:38:47 +0300

    Deposit Amount

--- project.js
+++ project.js
@@ -1,7 +1,27 @@
 //1. Deposit money
-//2.Determine number of lines on bet
-//3.Collect a bet amount
+//2. Determine number of lines on bet
+//3. Collect a bet amount
 //4. Spin the slot machine
 //5. check if the user won
 //6. give the user their winnings
 //7. play again
+
+const prompt = require("prompt-sync")();
+
+
+const deposit = () => {
+    while(true){
+    const depositAmount = prompt("Enter a deposit amount: ");
+    const numberDepositAmount = parseFloat(depositAmount);
+
+    if (isNaN(numberDepositAmount) || numberDepositAmount <= 0 ) {
+        console.log("Invalid deposit amount, try again");
+    }else {
+        return numberDepositAmount;
+    }
+    }
+};
+
+
+const depositAmount = deposit();
+console.log(depositAmount);
