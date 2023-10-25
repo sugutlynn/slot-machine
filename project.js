edff848ac3c04d1e55163876f1949f66bae978bf
sugutlynn <sugutlynn@gmail.com>
2023-10-25 23:23:48 +0300

    step 2 & 3

--- project.js
+++ project.js
@@ -8,6 +8,22 @@
 
 const prompt = require("prompt-sync")();
 
+const ROWS = 3;
+const COLS = 3;
+
+const SYMBOLS_COUNT = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8,
+}
+
+const SYMBOL_VALUES = {
+    "A": 5,
+    "B": 4,
+    "C": 3,
+    "D": 2,
+}
 
 const deposit = () => {
     while(true){
@@ -22,6 +38,37 @@ const deposit = () => {
     }
 };
 
+const getNumberOfLines = () => {
+    while(true){
+        const lines = prompt("Enter the number of lines to bet on (1-3): ");
+        const numberOfLines = parseFloat(lines);
+    
+        if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3 ) {
+            console.log("Invalid number of lines, try again");
+        }else {
+            return numberOfLines;
+        }
+        }
+}
 
-const depositAmount = deposit();
-console.log(depositAmount);
+const getBet = (balance, lines) => {
+    while(true){
+        const bet = prompt("Enter the bet per line: ");
+        const numberBet = parseFloat(bet);
+    
+        if (isNaN(numberBet) || numberBet <= 0 || numberBet > balance / lines ) {
+            console.log("Invalid bet, try again");
+        }else {
+            return numberBet;
+        }
+        }
+
+}
+
+
+let balance = deposit();
+console.log(balance);
+const numberOfLines = getNumberOfLines();
+console.log(numberOfLines);
+
+const bet = getBet(balance, numberOfLines);
