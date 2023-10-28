458dcdc04cfd87e64684a3bfa811093cd7845147
sugutlynn <sugutlynn@gmail.com>
2023-10-28 21:31:14 +0300

    transpose rows

--- project.js
+++ project.js
@@ -92,8 +92,24 @@ const spin = () => {
 
 };
 
+const transpose = (reels) => {
+    const rows = [];
+
+    for (let i = 0; i < ROWS; i++){
+        rows.push([]);
+        for (let j = 0; j < COLS; j++){
+            rows[i].push(reels[j][i])
+        }
+    }
+
+    return rows
+}
+
 const reels = spin();
 
 let balance = deposit();
 const numberOfLines = getNumberOfLines();
 const bet = getBet(balance, numberOfLines);
+
+const rows = transpose(reels);
+console.log(rows);
